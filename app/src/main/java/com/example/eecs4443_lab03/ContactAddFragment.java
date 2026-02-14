d175ef6f8945e494f8750792d4d89eae861902d7
Jorel Louie Chim <116990156+JorelC18@users.noreply.github.com>
2026-02-14 18:58:14 -0500

    Import change

--- app/src/main/java/com/example/eecs4443_lab03/ContactAddFragment.java
+++ app/src/main/java/com/example/eecs4443_lab03/ContactAddFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ContactAddFragment extends Fragment {
